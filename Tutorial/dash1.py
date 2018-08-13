f859d1d1c60fb573df91888485c3904d8f025506
Newton Galiza <newtonjgaliza@gmail.com>
2018-08-13 18:11:13 -0300

    Create dash1.py

---
+++ Tutorial/dash1.py
@@ -0,0 +1,24 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash()
+
+app.Layout = html.Div(children=[
+    html.H1('Dash Tutorials'),
+    dcc.Graph(
+        id='example',
+        figure={
+            'data': [
+                {'x': [1, 2, 3, 4, 5], 'y': [9, 6, 2, 1, 5], 'type': 'line', 'name': 'Boats'},
+                {'x': [1, 2, 3, 4, 5], 'y': [8, 7, 2, 7, 3], 'type': 'bar', 'name': 'Cars'},
+            ],
+            'layout': {
+                'title': 'Basic Dash Example'
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
